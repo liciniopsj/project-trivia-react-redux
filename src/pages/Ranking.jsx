35e26daf6992110a334aa6129bf44b6aac8c3abd
Otavio <suizld@pm.me>
2023-01-13 16:00:56 -0300

    Requisito 18

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Ranking extends React.Component {
+  handleHomeClick = () => {
+    const { history } = this.props;
+
+    history.push('/');
+  };
+
   render() {
     return (
-      <heading>
-        <h1 data-testid="ranking-title">Ranking</h1>
-      </heading>
+      <>
+        <heading>
+          <h1 data-testid="ranking-title">Ranking</h1>
+        </heading>
+        <section>
+          <button
+            type="button"
+            data-testid="btn-go-home"
+            onClick={ this.handleHomeClick }
+          >
+            Inicio
+          </button>
+        </section>
+      </>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.shape(),
+}.isRequered;
 
 export default Ranking;
