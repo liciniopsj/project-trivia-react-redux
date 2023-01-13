78092b089f08db039ba24eda90b60280f1bdbe1b
Otavio <suizld@pm.me>
2023-01-13 15:38:30 -0300

    Requisito 14

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -3,19 +3,21 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import Header from '../components/Header';
-import Main from '../components/Main';
 
 class Feedback extends React.Component {
   render() {
-    const { history, assertions } = this.props;
+    const { assertions, score } = this.props;
     const MIN_AFIRMATIONS = 3;
     return (
       <>
         <Header />
-        <Main history={ history } />
         <p data-testid="feedback-text">
           {assertions < MIN_AFIRMATIONS ? 'Could be better...' : 'Well Done!'}
         </p>
+        <section>
+          <p data-testid="feedback-total-score">{ score }</p>
+          <p data-testid="feedback-total-question">{ assertions }</p>
+        </section>
       </>
     );
   }
@@ -23,6 +25,7 @@ class Feedback extends React.Component {
 
 const mapStateToProps = (state) => ({
   assertions: state.player.assertions,
+  score: state.player.score,
 });
 
 Feedback.propTypes = {
